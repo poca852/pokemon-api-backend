3a736b47d1310f35f4742cf938207ad335449b4b
david cuspoca <cuspoca3d@gmail.com>
2022-05-04 13:26:54 -0600

    add postgres heroku

--- database/config.js
+++ database/config.js
@@ -1,8 +1,6 @@
 const {Sequelize} = require('sequelize');
 
-const sequelize = new Sequelize('pockemons', 'postgres', 'Au229497', {
-   host: 'localhost',
-   dialect: 'postgres',
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
    logging: false,
    native: false
 });
